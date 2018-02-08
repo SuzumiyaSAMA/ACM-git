f737ba7c317db035d0c17e9d1a87988f011bf0ff
SuzumiyaSAMA <17094536+SuzumiyaSAMA@users.noreply.github.com>
2018-02-08 20:47:49 +0800

    Create P1888.cpp

---
+++ luoGu/P1888.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+#include <iterator>
+using namespace std;
+int gcd(int a, int b){
+	if(b == 0)
+		return a;
+	else
+		return gcd(b, a%b);
+}
+int main(int argc, const char * argv[]) {
+	int iar[3] = {-1};
+	cin >> iar[0] >> iar[1] >> iar[2];
+	sort(begin(iar), end(iar));
+	int temp = gcd(iar[0], iar[2]);
+	iar[0] /= temp; iar[2]/= temp;
+	if(iar[2] % iar[0])
+		cout << iar[0] << '/' << iar[2];
+	else
+		cout << iar[0] << endl;
+	return 0;
+}
