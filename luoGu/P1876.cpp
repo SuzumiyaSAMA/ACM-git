9be9dc57d48751edfc76cd92efc309d06ac25576
SuzumiyaSAMA <17094536+SuzumiyaSAMA@users.noreply.github.com>
2018-05-07 16:52:15 +0800

    Create P1876.cpp

---
+++ luoGu/P1876.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+void way1(){										//an idoit method
+	bool light[10000000000000] = {false};
+	int N = -1;
+	bool flag = false;
+	cin >> N;
+	for(int count = 1; count <= N; count++){
+		for(int time = 1; time*count <= N; time++){
+			light[time*count] = !light[time*count];
+		}
+	}
+	for(int count = 0; count <= N; count++){
+		if(light[count]){
+			if(!flag){
+				cout << count;
+				flag = true;
+			}
+			else
+				cout << " " << count;
+		}
+	}
+}
+int main(int argc, const char * argv[]) {
+	int N = -1;
+	bool flag = false;
+	cin >>N;
+	for(int count = 1; count*count <= N; count++){
+		if(!flag){
+			cout << count*count;
+			flag = true;
+		}
+		else
+			cout << " " << count*count;
+	}
+	return 0;
+}
